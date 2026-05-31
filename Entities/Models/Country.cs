9013063ac8b2f8f18de2228737343e3976de09d6
aeldab3 <aeldabaa2@gmail.com>
2026-05-31 16:51:22 +0300

    Initial commit

---
+++ Entities/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Models
+{
+    /// <summary>
+    /// Domanin model for Country entity
+    /// </summary>
+    public class Country
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO for adding a new country.
+    /// </summary>
+    public class CountryAddRequest
+    {
+        public string? CountryName { get; set; }
+
+        public Country ToCountry()
+        {
+            return new Country
+            {
+                CountryName = this.CountryName
+            };
+        }
+    }
+}
