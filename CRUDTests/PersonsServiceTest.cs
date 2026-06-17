5691e0d19d6c3251dcca3efa4459fc40b6adf7a3
aeldab3 <aeldabaa2@gmail.com>
2026-06-17 23:59:43 +0300

    feat(PersonsServiceTest): add xUnit tests for Persons

---
+++ CRUDTests/PersonsServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUDTests
+{
+    public class PersonsServiceTest
+    {
+        private readonly IPersonService _personService;
+
+        public PersonsServiceTest()
+        {
+            _personService = new PersonService();
+        }
+
+
+        // When Supply Null Value
+        [Fact]
+        public void AddPerson_NullPerson()
+        {
+            // Arrange
+            PersonAddRequest? personAddRequest = null;
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                _personService.AddPerson(personAddRequest);
+            });
+        }
+
+        // When Supply Null Value for personName
+        [Fact]
+        public void AddPerson_NullPersonName()
+        {
+            // Arrange
+            PersonAddRequest? personAddRequest = new PersonAddRequest()
+            {
+                PersonName = null
+            };
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                _personService.AddPerson(personAddRequest);
+            });
+        }
+
+
+        // When Supply proper person details, and add new person to persons list
+        [Fact]
+        public void AddPerson_ProperPersonDetails()
+        {
+            // Arrange
+            PersonAddRequest? personAddRequest = new PersonAddRequest()
+            {
+                PersonName = "Ahmed",
+                Email = "a@gmail.com",
+                Address = "Kafr El-Zayay",
+                DateOfBirth = DateTime.Parse("1999-01-18"),
+                Gender = GenderOptions.Male,
+                CountryId = Guid.NewGuid(),
+                ReceiveNewsLetters = true
+            };
+
+            // Act
+            PersonResponse personResponse = _personService.AddPerson(personAddRequest);
+            List<PersonResponse> personsList =  _personService.GetAllPersons();
+
+            // Assert
+            Assert.True(personResponse.PersonID != Guid.Empty);
+            Assert.Contains(personResponse, personsList);
+
+        }
+
+    }
+}
---
+++ ServiceContracts/IPersonService.cs
@@ -0,0 +1,22 @@
+﻿using ServiceContracts.DTO;
+using System;
+
+namespace ServiceContracts
+{
+    public interface IPersonService
+    {
+        /// <summary>
+        /// Adds a new person to the data store.
+        /// </summary>
+        /// <param name="personRequest"></param>
+        /// <returns>The added person as a PersonResponse object.</returns>
+        PersonResponse AddPerson(PersonAddRequest? personRequest);
+
+
+        /// <summary>
+        /// Retrieves all persons from the data store.
+        /// </summary>
+        /// <returns>A list of all persons as PersonResponse objects.</returns>
+        List<PersonResponse> GetAllPersons();
+    }
+}
