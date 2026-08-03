ef3e2cf9ba3d88246baa0581ab70cf5376d3f5a6
aeldab3 <aeldabaa2@gmail.com>
2026-08-04 00:17:05 +0300

    feat: add PersonsListActionFilter that is a filter

---
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ActionFilters
+{
+    public class PersonsListActionFilter : IActionFilter
+    {
+        private readonly ILogger<PersonsListActionFilter> _logger;
+        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            _logger.LogInformation("PersonsListActionFilter OnActionExecutedContext");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInformation("PersonsListActionFilter OnActionExecutingContext");
+        }
+    }
+}
