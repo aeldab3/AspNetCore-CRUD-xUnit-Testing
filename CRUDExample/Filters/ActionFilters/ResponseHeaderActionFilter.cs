c94a5b86a2065517ca70d3cdea9259cb5bd0fbd9
aeldab3 <aeldabaa2@gmail.com>
2026-08-09 17:04:42 +0300

    feat: add new Reasponse header filter and use filter arguments

--- CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog.Core;
 
 namespace CRUDExample.Filters.ActionFilters
 {
-    public class PersonsListActionFilter : IActionFilter
+    public class ResponseHeaderActionFilter : IActionFilter
     {
-        private readonly ILogger<PersonsListActionFilter> _logger;
-        public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+        private readonly ILogger<ResponseHeaderActionFilter> _logger;
+        private readonly string _key;
+        private readonly string _value;
+        public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value)
         {
             _logger = logger;
+            _key = key;
+            _value = value;
         }
-
         public void OnActionExecuted(ActionExecutedContext context)
         {
             _logger.LogInformation("{FilterNeme}.{MethodName] method",
@@ -24,6 +28,8 @@ namespace CRUDExample.Filters.ActionFilters
                 nameof(PersonsListActionFilter),
                 nameof(OnActionExecuting)
             );
+
+            context.HttpContext.Response.Headers[_key] = _value; 
         }
     }
 }
